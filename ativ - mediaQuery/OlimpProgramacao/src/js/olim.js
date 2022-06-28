5b4a1502c085ee41614ce581f352bd181359cf93
Thainara <thainarapenha@alu.ufc.br>
2022-06-28 14:29:44 -0300

    criei tela de login e implementei lógica pra troca de telas

---
+++ ativ - mediaQuery/OlimpProgramacao/src/js/olim.js
@@ -0,0 +1,9 @@
+$("#btnInicio").click(() => {
+    $("#mainHome").attr('style', '');
+    $("#Login").attr('style', 'display: none');
+})
+
+$("#btnLogin").click(() => {
+    $("#Login").attr('style', '');
+    $("#mainHome").attr('style', 'display: none');
+})
