45f75fd7bdfcff63d7959091360119c28d9f8c8f
Thainara <thainarapenha@alu.ufc.br>
2022-06-28 15:18:11 -0300

    criei tela de inscrição na olimpiada e finalizei lógica de troca de páginas

--- ativ - mediaQuery/OlimpProgramacao/src/js/olim.js
+++ ativ - mediaQuery/OlimpProgramacao/src/js/olim.js
@@ -1,9 +1,15 @@
 $("#btnInicio").click(() => {
     $("#mainHome").attr('style', '');
     $("#Login").attr('style', 'display: none');
+    $("#Registro").attr('style', 'display: none');
 })
-
 $("#btnLogin").click(() => {
     $("#Login").attr('style', '');
     $("#mainHome").attr('style', 'display: none');
+    $("#Registro").attr('style', 'display: none');
+})
+$("#btnRegistrar").click(() => {
+    $("#Registro").attr('style', '');
+    $("#mainHome").attr('style', 'display: none');
+    $("#Login").attr('style', 'display: none');
 })
